ba7cfa0aa3268c1312d69035142b16a7bdac8d54
Bobobek <murodovbobobek0412@gmail.com>
2025-11-21 22:08:43 +0500

    Add __init__.py and fix data_loader import

--- data_loader.py
+++ data_loader.py
@@ -1,15 +1,11 @@
 import importlib
 
 def load_words(book: str, gwa: int):
-    book = book.upper()
-
-    # Hozircha faqat 1A
-    if book != "1A":
-        return []
+    module_name = f"book_{book}.gwa{gwa}"
 
     try:
-        module = importlib.import_module(f"book_1A.gwa{gwa}")
+        module = importlib.import_module(module_name)
         return module.WORDS
     except Exception as e:
-        print("Import error:", e)
+        print("ERROR:", e)
         return []
